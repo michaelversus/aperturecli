25d6a915ca87c8b531feecbc7287f366a8095f1d
Michalis Karagiorgos <m.karagiorgos@kaizengaming.com>
2026-03-10 08:37:53 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ApertureCLI",
+    targets: [
+        .executableTarget(
+            name: "ApertureCLI"
+        ),
+    ]
+)
